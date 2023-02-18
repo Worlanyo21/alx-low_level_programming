69efb09f3dac68c613e1da3e0ab488fce64166b9
Worlanyo21 <wwagordo001@st.ug.edu.gh>
2023-02-17 22:05:03 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int a;
 
-	for (a = 0, a < 10; a++)
+	for (a = 0; a < 10; a++)
 	{
-		puchar(a + '0');
+		putchar(a + '0');
 		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n);
+	putchar('\n');
 	return (0);
 }
